92305266f730f0a184f5b244da6a8d75db80418e
Bradley Biehl <bradley.biehl@gmail.com>
2019-05-18 13:08:13 -0500

    tested create authors happy path

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
+  context 'all required fields are present' do
+    subject { Author.create(name: 'Michael McDonald') }
+
+    it 'is not valid' do
+      expect(subject).to be_valid
+    end
+  end
+
   context 'name is not present' do
     subject { Author.create(name: '') }
 
